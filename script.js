2cd07b9540320e5023657d29d20415730bbc3616
IAmShubhamSahay <119969435+IAmShubhamSahay@users.noreply.github.com>
2023-03-07 22:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function palindrome(str){
     char ch ;
 	String s="";
-	for (let index = str.length(); index >=0 ; index++) {
+	for (let index = (str.length()-1); index >=0 ; index++) {
 		ch=str.charAt(index);
 		s=s+ch;
 	}
