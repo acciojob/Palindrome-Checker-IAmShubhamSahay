3141de4f9290a60be2bbcb80e6591cbea502ba9e
IAmShubhamSahay <119969435+IAmShubhamSahay@users.noreply.github.com>
2023-03-07 22:37:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+    char ch ;
+	String s="";
+	for (let index = str.length(); index >=0 ; index++) {
+		ch=str.charAt(index);
+		s=s+ch;
+	}
+	if(s==str){
+		return true;
+	}else{
+		return false;
+	}
 }
 module.exports = palindrome
